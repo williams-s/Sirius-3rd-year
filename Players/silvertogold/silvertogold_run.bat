e56b94ea87352c338c3db3b319f86724376d9d7e
Younes-Git <149156274+Younes-Git@users.noreply.github.com>
2025-12-04 00:17:31 +0100

    Delete Players directory

--- Players/silvertogold/silvertogold_run.bat
+++
@@ -1,3 +0,0 @@
-%~d0
-cd %~dp0
-java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.13.2.jar;../lib/log4j-api-2.13.2.jar;../lib/log4j-core-2.13.2.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/dom4j-2.1.3.jar;../lib/mongo-java-driver-3.12.0.jar;../lib/postgresql-42.2.14.jar;../lib/slf4j-api-1.7.29.jar;../lib/crypto-utils-0.31.12.jar;silvertogold_0_1.jar; local_project.silvertogold_0_1.silvertogold --context=Default %*
