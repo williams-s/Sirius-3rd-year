523e97a55bdec0c759f0cb5c8322218e7d91c18e
Williams Anthony <willay.anthony@gmail.com>
2026-01-02 17:48:33 +0100

    Creating backends

---
+++ entrance-cockpit/src/test/java/club/manager/entrance_cockpit/EntranceCockpitApplicationTests.java
@@ -0,0 +1,13 @@
+package club.manager.entrance_cockpit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceCockpitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ team-performance/src/main/java/club/manager/team_performance/TeamPerformanceApplication.java
@@ -0,0 +1,13 @@
+package club.manager.team_performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamPerformanceApplication.class, args);
+	}
+
+}
