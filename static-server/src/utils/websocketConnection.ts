8ab7fb5d188cd443e46ea9f680f8febbe70fed30
Williams Anthony <willay.anthony@gmail.com>
2026-02-14 15:25:49 +0100

    Https for reverse proxy

--- static-server/src/utils/websocketConnection.ts
+++ static-server/src/utils/websocketConnection.ts
@@ -2,10 +2,6 @@ import {WEBSOCKET_URL} from "../constants/back.ts";
 import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
-export const ConnectToWebsocket = (): WebSocket => {
-    return new WebSocket(WEBSOCKET_URL);
-}
-
 export const ConnectToWebSocketSTOMP = (): Client => {
     return new Client({
         webSocketFactory: () => new SockJS(WEBSOCKET_URL),
